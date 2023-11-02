68c159c18a35bf310dced97c0dcb7e93e1ea191e
jalynsnowflake <149723952+jalynsnowflake@users.noreply.github.com>
2023-11-02 15:07:41 -0600

    Update streamlit_app.py
    Added smiley face

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-streamlit.title( 'My Parents New Health Diner')
+streamlit.title( 'My Mom's New Healthy Diner')
 
-streamlit.header('Breakfast Favorites')
+streamlit.header(:smile: 'Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
+streamlit.header('Build Your Own Fruit Smoothie')
