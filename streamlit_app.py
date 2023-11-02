f86f7cae0059d491e6416afa70560cb2e48805fa
jalynsnowflake <149723952+jalynsnowflake@users.noreply.github.com>
2023-11-02 15:13:47 -0600

    Update streamlit_app.py
    Corrected emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit
 
 streamlit.title( 'My Mom\'s New Healthy Diner')
 
-streamlit.header('🥣 Breakfast Favorites')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('Build Your Own Fruit Smoothie')
