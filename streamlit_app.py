2ebe540e40af083142df26f56f5a2b249d24f381
jalynsnowflake <149723952+jalynsnowflake@users.noreply.github.com>
2023-11-02 15:10:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title( 'My Mom\'\s New Healthy Diner')
 
-streamlit.header(:smile: 'Breakfast Favorites')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
